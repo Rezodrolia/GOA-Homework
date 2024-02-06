7280ec7f17ddd0c9a77b913955c7413a423077b9
Rezodrolia <sprictur@gmail.com>
2024-02-06 23:03:24 +0400

    kumalala master

---
+++ Day 2/kumalala kumalala savesta.py
@@ -0,0 +1,4 @@
+print("I was in GOA voicechats before i even attended the first lesson, i've already made a ton of friends, too much to list" )
+
+print("I think GOA is a good place for me and for my self-improvement journey which i've started a long time ago, even though i do disagree with some statements made by the main teacher.")
+#I've already subbed to the channel a LONG time ago(2 years), went and hugged my mother, i've completed the homework.
