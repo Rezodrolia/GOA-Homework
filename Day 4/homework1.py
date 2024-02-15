7ecb80378c6e5708e227b5e87877305a3bcc15df
Rezodrolia <sprictur@gmail.com>
2024-02-15 23:11:43 +0400

    turtlehouse

---
+++ Day 4/homework1.py
@@ -0,0 +1,8 @@
+name = "Nika"
+lastname = "Zaridze"
+Age = 16
+futureme = Age + 10
+
+
+print("Hello, my name is ",name, lastname , " And i am " , Age , 
+      " years old. In 10 years i will be " , futureme , " years old.")
