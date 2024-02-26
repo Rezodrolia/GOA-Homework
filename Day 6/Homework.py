cf31edbd90facdb2c28894dd455d20928a90b7f9
Rezodrolia <sprictur@gmail.com>
2024-02-26 19:18:19 +0400

    commit

---
+++ Day 6/Homework.py
@@ -0,0 +1,4 @@
+first_name = input("Enter name")
+surname = input("Enter surname")
+age = input("Enter age")
+print(" Statistics include ",first_name,surname,age)
---
+++ Day 6/Homework1.py
@@ -0,0 +1,9 @@
+
+#First let's make the variables where we will put the numbers for our calculation
+number_1 = int(input("Type in first number here "))
+number_2 = int(input("Type in second number here "))
+number_3 = int(input ("Type in third number here "))
+#Now we can calculate using them
+print("Calculated : ",number_1 - number_3)
+print("Calculated : ",number_1 * number_2)
+print("Calculated : ",number_1 / number_2)
